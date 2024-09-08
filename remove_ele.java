d2854f97ce7af1347aedb153ea2a14b8d00365ef
Zuveriya Munshi <zuveriyamunshi924@gmail.com>
2024-09-08 18:26:05 +0530

    remove element from array

--- remove_ele.java
+++ remove_ele.java
@@ -1,12 +1,13 @@
+import java.util.*;
 class remove_ele {
-    public int removeElement(int[] nums, int val) {
+    public static int removeElement(int[] nums, int val) {
         int len = nums.length;
         int i=0;
-        while(ilen){
+        while(i<len){
             if(nums[i]==val){
-                 Move last element to the place where the val matched
+                // Move last element to the place where the val matched
                 nums[i] = nums[len-1];
-                decrease the length of array
+                //decrease the length of array
                 len--;
             }
             else{
@@ -15,4 +16,15 @@ class remove_ele {
         }
         return len;
     }
+	public static void main(String []args){
+	Scanner s = new Scanner(System.in);
+	int n = s.nextInt(); // number of elements
+	int [] nums = new int[n];
+	for(int i=0;i<n;i++){
+		nums[i] = s.nextInt(); 
+	}
+	int val = s.nextInt(); // value to be removed from array
+	int len = removeElement(nums,val);	
+	System.out.println("Length of Array : " + len);
+}
 }
